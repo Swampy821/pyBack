5fa44419e5ddec1b43a621f7d22f2f8dffb0948e
Steve Marsh <Zimdale@gmail.com>
2014-02-01 14:39:21 -0500

    Added logging

--- backup.py
+++ backup.py
@@ -1,5 +1,13 @@
+import os, os.path, sys
 pmName = 'config/dirConfig.py'
 execfile(pmName)
-
-
+dirtocheck = '/home/steve/pythonProjects/'
+for root, _, files in os.walk(dirtocheck):
+	for f in files:
+		fullpath = os.path.join(root, f)
+		sys.stdout.write("%s \n" % fullpath);
+		f = open("dirList.txt",'a')
+		f.write(fullpath+'\n');
+		f.close()
+#Logging files. Project may just turn into a project logger. Make new Repo.
 #Copy Files from config to backup directory here.
